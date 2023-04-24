f2c4cc959e363c1f4d76dc1dfab635530652e2ea
Blacky04042019 <110211024+Blacky04042019@users.noreply.github.com>
2023-04-24 19:27:03 -0400

    Update MLBSB.py

--- MLBSB.py
+++ MLBSB.py
@@ -50,5 +50,3 @@ main_slate_lineup = create_lineup(50000, "main")
 # Create an express slate lineup
 express_slate_lineup = create_lineup(25000, "express")
 
-# Create an after hours slate lineup
-after_hours_slate_lineup = create_lineup(10000, "after_hours"
